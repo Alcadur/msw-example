72942b0c7a3ebe6da2ce7cfa16bf6a4a6f9831a0
Piotr Chełmicki <p.chelmicki@merixstudio.com>
2026-06-29 07:44:45 +0200

    add mocked handler for add meal to cart

--- msw-react/src/mocks/handlers.ts
+++ msw-react/src/mocks/handlers.ts
@@ -1,8 +1,17 @@
-import { http, HttpResponse } from 'msw'
+import { delay, http, HttpResponse } from "msw";
 import { Meals } from "@/src/mocks/mockData/meals";
 
 export const handlers = [
     http.get('/api/meals', () => {
         return HttpResponse.json(Meals)
+    }),
+    http.post<{ id: string }>('/api/meals/add/:id', async ({ params }) => {
+        await delay(850)
+
+        if (params.id === '2') {
+            return new HttpResponse(null, { status: 425 })
+        }
+
+        return new HttpResponse(null, { status: 202 })
     }),
 ]
